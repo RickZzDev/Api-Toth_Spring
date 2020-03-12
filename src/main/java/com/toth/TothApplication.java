833c0bf44ca884a1801b051cbc8f123b080cbff7
Carlos <rick_teloon@hotmail.com>
2020-03-12 15:38:57 -0300

    Subindo second commit

---
+++ src/main/java/com/toth/TothApplication.java
@@ -0,0 +1,13 @@
+package com.toth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toth/TothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
