1513ef52c1161222bb04b58470addbfe1c8c6a42
Carlos <rick_teloon@hotmail.com>
2020-07-14 17:56:31 -0300

    Puxando comunicados

--- src/main/java/com/toth/resource/ComunicadoEscolaResource.java
+++ src/main/java/com/toth/resource/ComunicadoEscolaResource.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/comunicados-escola")
+@RequestMapping("/comunicados/escola")
 public class ComunicadoEscolaResource {
 
     @Autowired
